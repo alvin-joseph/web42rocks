c5072a1148418e6fefdc27d96c136174c6d95283
alvin-joseph <aznman1234@gmail.com>
2021-06-03 12:20:11 -0500

    edit index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const server = express();
 
+if (process.env.NODE_ENV === 'production') {
+    console.log('this means this code is deployed');
+  }
+
 const PORT = process.env.PORT || 5000;
 
 server.get('/api', (req, res) => {
