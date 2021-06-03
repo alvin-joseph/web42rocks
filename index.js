63ca72aff2e15902a8c6a891051782a5e350f180
alvin-joseph <aznman1234@gmail.com>
2021-06-03 12:59:00 -0500

    added react

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
 const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use(express.static(path.join(__dirname, 'client/build'))); //static assets
+
 
 if (process.env.NODE_ENV === 'production') {
     console.log('this means this code is deployed');
   }
 
 const PORT = process.env.PORT || 5000;
+
+server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 server.get('/api', (req, res) => {
     res.json({ message: `${process.env.COHORT} ROCKS`});
